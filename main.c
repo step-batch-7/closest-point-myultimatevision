15f97afb0faee93372dd6f4083012129b81de758
nagendra <=>
2020-04-13 11:38:57 +0530

    changing closest_food according to distance in get_closest_food

--- main.c
+++ main.c
@@ -5,10 +5,9 @@ int main(void)
 {
   struct Point food_points[] = {{18, 76}, {19, 66}, {89, 57}, {9, 71}, {55, 38}};
   int points_length = 5;
-  struct Point current_location = {20, 70};
+  struct Point current_location = {69, 7};
   struct Point closest_food_location = {18, 76};
   get_closest_food(food_points, points_length, current_location, &closest_food_location);
-  printf("%d\n", closest_food_location.x);
-  printf("%d\n", closest_food_location.y);
+  printf("%d %d\n", closest_food_location.x, closest_food_location.y);
   return 0;
 }
