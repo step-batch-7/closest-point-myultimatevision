8150e0f4e64b597ad7e53066d560a353936883b5
nagendra <=>
2020-04-13 11:52:41 +0530

    added #define for struct Point

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "point.h"
 
+#define POINT struct Point
+
 int main(void)
 {
-  struct Point food_points[] = {{18, 76}, {19, 66}, {89, 57}, {9, 71}, {55, 38}};
+  POINT food_points[] = {{18, 76}, {19, 66}, {89, 57}, {9, 71}, {55, 38}};
   int points_length = 5;
-  struct Point current_location = {69, 7};
-  struct Point closest_food_location = {18, 76};
+  POINT current_location = {69, 7};
+  POINT closest_food_location = {18, 76};
   get_closest_food(food_points, points_length, current_location, &closest_food_location);
   printf("%d %d\n", closest_food_location.x, closest_food_location.y);
   return 0;
