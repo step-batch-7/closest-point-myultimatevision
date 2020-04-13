e73e4002faf30b4c3eb53c3d75c2eb5d8abf295f
nagendra <=>
2020-04-13 12:51:55 +0530

    modifying distance_from_closest_food inside the if statement

--- point.c
+++ point.c
@@ -11,15 +11,16 @@ double find_distance_between(POINT point_a, POINT point_b)
 
 void get_closest_food(POINT food_points[], int points_length, POINT current_location, POINT *closest_food_location)
 {
-  double distance_from_current_food, distance_from_closest_food;
+  double distance_from_current_food;
+  double distance_from_closest_food = find_distance_between(current_location, *closest_food_location);
   for (int i = 0; i < points_length; i++)
   {
     double distance_from_current_food = find_distance_between(current_location, food_points[i]);
-    double distance_from_closest_food = find_distance_between(current_location, *closest_food_location);
     if (distance_from_current_food < distance_from_closest_food)
     {
       closest_food_location->x = food_points[i].x;
       closest_food_location->y = food_points[i].y;
+      distance_from_closest_food = distance_from_current_food;
     }
   }
 }
