7de676ac20dc637562733a346b653a63be1b3004
nagendra <=>
2020-04-13 11:26:36 +0530

    added get_closest_food function to point.c

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
+#include <stdio.h>
 #include "point.h"
 
 int main(void)
 {
+  struct Point food_points[] = {{18, 76}, {19, 66}, {89, 57}, {9, 71}, {55, 38}};
+  int points_length = 5;
+  struct Point current_location = {20, 70};
+  struct Point closest_food_location = {18, 76};
+  get_closest_food(food_points, points_length, current_location, &closest_food_location);
+  printf("%d\n", closest_food_location.x);
+  printf("%d\n", closest_food_location.y);
   return 0;
 }
---
+++ point.c
@@ -0,0 +1,6 @@
+#include <math.h>
+#include "point.h"
+
+void get_closest_food(struct Point food_points[], int points_length, struct Point current_location, struct Point *closest_food_location)
+{
+}
