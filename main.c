5d96deb013c787f5681f75bb580d0a31850daf78
nagendra <=>
2020-04-13 19:52:34 +0530

    moved initialisation of closest_food_location to point.c

--- main.c
+++ main.c
@@ -7,8 +7,8 @@ int main(void)
 {
   POINT food_points[] = {{18, 76}, {19, 66}, {89, 57}, {9, 71}, {55, 38}};
   int points_length = 5;
-  POINT current_location = {97, 27};
-  POINT closest_food_location = {18, 76};
+  POINT current_location = {69, 7};
+  POINT closest_food_location;
   get_closest_food(food_points, points_length, current_location, &closest_food_location);
   print_organism_and_food(current_location, closest_food_location);
   return 0;
--- point.c
+++ point.c
@@ -13,13 +13,13 @@ void get_closest_food(POINT food_points[], int points_length, POINT current_loca
 {
   double distance_from_current_food;
   double distance_from_closest_food = find_distance_between(current_location, *closest_food_location);
-  for (int i = 0; i < points_length; i++)
+  *closest_food_location = food_points[0];
+  for (int i = 1; i < points_length; i++)
   {
     double distance_from_current_food = find_distance_between(current_location, food_points[i]);
     if (distance_from_current_food < distance_from_closest_food)
     {
-      closest_food_location->x = food_points[i].x;
-      closest_food_location->y = food_points[i].y;
+      *closest_food_location = food_points[i];
       distance_from_closest_food = distance_from_current_food;
     }
   }
